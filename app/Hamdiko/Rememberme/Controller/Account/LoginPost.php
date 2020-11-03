4c4619018a542b47bcdf6bc49f579ead3896549a
Moahmmad Hamdy <mhamdy@awstreams.com>
2020-11-03 12:01:03 +0200

    first push

---
+++ app/Hamdiko/Rememberme/Controller/Account/LoginPost.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohammad Hamdy
+ * Date: 11/2/2020
+ * Time: 12:20 PM
+ */
+
+namespace Hamdiko\Rememberme\Controller\Account;
+
+use Magento\Customer\Model\Account\Redirect as AccountRedirect;
+use Magento\Framework\App\Action\Context;
+use Magento\Customer\Model\Session;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Model\Url as CustomerUrl;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Exception\EmailNotConfirmedException;
+use Magento\Framework\Exception\AuthenticationException;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\State\UserLockedException;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Hamdiko\Rememberme\Helper\Data as CustomCooike;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\Cookie\PhpCookieManager;
+
+class LoginPost extends \Magento\Customer\Controller\Account\LoginPost
+{
+    /**
+     * @var AccountManagementInterface
+     */
+    protected $customerAccountManagement;
+
+    /**
+     * @var Validator
+     */
+    protected $formKeyValidator;
+
+    /**
+     * @var AccountRedirect
+     */
+    protected $accountRedirect;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory
+     */
+    private $cookieMetadataFactory;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\PhpCookieManager
+     */
+    private $cookieMetadataManager;
+
+    /**
+     * @var \Hamdiko\Rememberme\Helper\Data
+     */
+    private $getCookiedata;
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param CustomerUrl $customerHelperData
+     * @param Validator $formKeyValidator
+     * @param AccountRedirect $accountRedirect
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        AccountManagementInterface $customerAccountManagement,
+        CustomerUrl $customerHelperData,
+        Validator $formKeyValidator,
+        AccountRedirect $accountRedirect,
+        CustomCooike $getCookiedata
+    ) {
+        $this->session = $customerSession;
+        $this->customerAccountManagement = $customerAccountManagement;
+        $this->customerUrl = $customerHelperData;
+        $this->formKeyValidator = $formKeyValidator;
+        $this->accountRedirect = $accountRedirect;
+        $this->getCookiedata = $getCookiedata;
+        parent::__construct($context, $customerSession, $customerAccountManagement, $customerHelperData, $formKeyValidator, $accountRedirect);
+    }
+
+    /**
+     * Get scope config
+     *
+     * @return ScopeConfigInterface
+     * @deprecated 100.0.10
+     */
+    private function getScopeConfig()
+    {
+        if (!($this->scopeConfig instanceof \Magento\Framework\App\Config\ScopeConfigInterface)) {
+            return \Magento\Framework\App\ObjectManager::getInstance()->get(
+                \Magento\Framework\App\Config\ScopeConfigInterface::class
+            );
+        } else {
+            return $this->scopeConfig;
+        }
+    }
+    /**
+     * Retrieve cookie manager
+     *
+     * @deprecated
+     * @return PhpCookieManager
+     */
+    private function getCookieManager()
+    {
+        if (!$this->cookieMetadataManager) {
+            $this->cookieMetadataManager = ObjectManager::getInstance()->get(
+                PhpCookieManager::class
+            );
+        }
+        return $this->cookieMetadataManager;
+    }
+    /**
+     * Retrieve cookie metadata factory
+     *
+     * @deprecated
+     * @return CookieMetadataFactory
+     */
+    private function getCookieMetadataFactory()
+    {
+        if (!$this->cookieMetadataFactory) {
+            $this->cookieMetadataFactory = ObjectManager::getInstance()->get(
+                CookieMetadataFactory::class
+            );
+        }
+        return $this->cookieMetadataFactory;
+    }
+    /**
+     * Login post action
+     *
+     * @return Redirect
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function execute()
+    {
+        if ($this->session->isLoggedIn() || !$this->formKeyValidator->validate($this->getRequest())) {
+            /** @var Redirect $resultRedirect */
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $resultRedirect->setPath('*/*/');
+            return $resultRedirect;
+        }
+
+        if ($this->getRequest()->isPost()) {
+            $login = $this->getRequest()->getPost('login');
+            if (!empty($login['username']) && !empty($login['password'])) {
+                try {
+                    if (array_key_exists('rememberme', $login)) {
+                        $logindetails = array('username' => $login['username'], 'password' => $login['password'], 'rememberme' => true);
+                        $logindetails = json_encode($logindetails);
+
+                        $this->getCookiedata->set($logindetails, $this->getCookiedata->getCookielifetime());
+                    } else {
+                        $this->getCookiedata->delete('rememberme');
+                    }
+
+
+                    $customer = $this->customerAccountManagement->authenticate($login['username'], $login['password']);
+                    $this->session->setCustomerDataAsLoggedIn($customer);
+                    $this->session->regenerateId();
+                    if ($this->getCookieManager()->getCookie('mage-cache-sessid')) {
+                        $metadata = $this->getCookieMetadataFactory()->createCookieMetadata();
+                        $metadata->setPath('/');
+                        $this->getCookieManager()->deleteCookie('mage-cache-sessid', $metadata);
+                    }
+                    $redirectUrl = $this->accountRedirect->getRedirectCookie();
+                    if (!$this->getScopeConfig()->getValue('customer/startup/redirect_dashboard') && $redirectUrl) {
+                        $this->accountRedirect->clearRedirectCookie();
+                        $resultRedirect = $this->resultRedirectFactory->create();
+                        // URL is checked to be internal in $this->_redirect->success()
+                        $resultRedirect->setUrl($this->_redirect->success($redirectUrl));
+                        return $resultRedirect;
+                    }
+                } catch (EmailNotConfirmedException $e) {
+                    $value = $this->customerUrl->getEmailConfirmationUrl($login['username']);
+                    $message = __(
+                        'This account is not confirmed. <a href="%1">Click here</a> to resend confirmation email.',
+                        $value
+                    );
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
+                } catch (UserLockedException $e) {
+                    $message = __(
+                        'The account is locked. Please wait and try again or contact %1.',
+                        $this->getScopeConfig()->getValue('contact/email/recipient_email')
+                    );
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
+                } catch (AuthenticationException $e) {
+                    $message = __('Invalid login or password.');
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
+                } catch (LocalizedException $e) {
+                    $message = $e->getMessage();
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
+                } catch (\Exception $e) {
+                    // PA DSS violation: throwing or logging an exception here can disclose customer password
+                    $this->messageManager->addError(
+                        __('An unspecified error occurred. Please contact us for assistance.')
+                    );
+                }
+            } else {
+                $this->messageManager->addError(__('A login and a password are required.'));
+            }
+        }
+
+        return $this->accountRedirect->getRedirect();
+    }
+}
---
+++ app/Hamdiko/Rememberme/Helper/Data.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohammad Hamdy
+ * Date: 11/2/2020
+ * Time: 12:25 PM
+ */
+
+namespace Hamdiko\Rememberme\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Framework\Session\SessionManagerInterface;
+
+class Data extends AbstractHelper
+{
+    /**
+     * Name of Cookie that holds private content version
+     */
+    CONST COOKIE_NAME = 'rememberme';
+
+    /**
+     * Cookie life time
+     */
+    CONST COOKIE_LIFE = 604800;
+
+    /**
+     * @var CookieManagerInterface
+     */
+    protected $cookieManager;
+
+    /**
+     * @var CookieMetadataFactory
+     */
+    protected $cookieMetadataFactory;
+
+    /**
+     * @var $scopeConfigInterface
+     */
+    private $scopeConfigInterface;
+
+    /**
+     * @var SessionManagerInterface
+     */
+    protected $sessionManager;
+
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfigInterface,
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory,
+        SessionManagerInterface $sessionManager
+    ){
+        $this->scopeConfigInterface = $scopeConfigInterface;
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+        $this->sessionManager = $sessionManager;
+    }
+
+    /**
+     * Get data from cookie set in remote address
+     *
+     * @return value
+     */
+    public function get($name)
+    {
+        return $this->cookieManager->getCookie($name);
+    }
+
+    /**
+     * Set data to cookie in remote address
+     *
+     * @param [string] $value    [value of cookie]
+     * @param integer $duration [duration for cookie] 7 Days
+     *
+     * @return void
+     */
+    public function set($value, $duration = 604800)
+    {
+        $metadata = $this->cookieMetadataFactory
+            ->createPublicCookieMetadata()
+            ->setDuration($duration)
+            ->setPath($this->sessionManager->getCookiePath())
+            ->setDomain($this->sessionManager->getCookieDomain());
+
+        $this->cookieManager->setPublicCookie(self::COOKIE_NAME, $value, $metadata);
+
+    }
+
+    /**
+     * delete cookie remote address
+     *
+     * @return void
+     */
+    public function delete($name)
+    {
+        $this->cookieManager->deleteCookie(
+            $name,
+            $this->cookieMetadataFactory
+                ->createCookieMetadata()
+                ->setPath($this->sessionManager->getCookiePath())
+                ->setDomain($this->sessionManager->getCookieDomain())
+        );
+    }
+
+    /**
+     * @return Data
+     */
+    public function getCookieloginName()
+    {
+        $name = json_decode($this->get(self::COOKIE_NAME));
+        if($name)
+            return $name->username ? $name->username : '';
+    }
+
+    /**
+     * @return Data
+     */
+    public function getCookieloginPwd()
+    {
+        $pwd = json_decode($this->get(self::COOKIE_NAME));
+        if($pwd)
+            return $pwd->password ? $pwd->password : '';
+    }
+
+    /**
+     * @return Data
+     */
+    public function getCookieloginChk()
+    {
+        $chk = json_decode($this->get(self::COOKIE_NAME));
+        if($chk)
+            return $chk->rememberme ? true : '';
+    }
+
+    /**
+     * @return var
+     */
+    public function getCookielifetime()
+    {
+        return self::COOKIE_LIFE;
+    }
+}
